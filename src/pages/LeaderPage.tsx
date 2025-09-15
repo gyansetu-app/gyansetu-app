cc84b1da4eb06e9245fe1ed87604da5b9fb2c51f
Punay kukreja <kukrejapunay04@gmail.com>
2025-09-15 22:14:52 +0530

    changed leaderboard

--- src/pages/LeaderPage.tsx
+++ src/pages/LeaderPage.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { Button } from '@/components/ui/button';
-import React from 'react'
 
 const LeaderPage = () => {
   return (
     <div className=' px-2 py-4'>
         <div>
             <Button className='w-full h-32 shadow-none'>
-                leaderboard
+                leaderboard here
             </Button>
         </div>
         <div className='h-full w-full'>
