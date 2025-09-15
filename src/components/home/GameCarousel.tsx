e15330480fe55f18c70d275694720bec1177131b
hyplona <hyplonax@gmail.com>
2025-09-16 00:05:39 +0530

    added 4th slider in homepage

--- src/components/home/GameCarousel.tsx
+++ src/components/home/GameCarousel.tsx
@@ -2,29 +2,52 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 
-import ImageCard from '@/components/ui/image-card'
+import ImageCard from "@/components/ui/image-card";
 
 export default function GameCarousel() {
-  const captions = [
-    "Rasayan: Compound Building Game",
-    "Ballebaaz: Trigonometry Puzzle",
-    "Aanganwadi: Virtual Village",
-    "Experimental Fun Lab"
-  ]
-  return <Carousel className="w-full">
-    <CarouselContent>
-      {Array.from({ length: 4 }).map((_, index) => (
-        <CarouselItem key={index} >
-          <div className="p-[10px]">
-            <ImageCard
-              caption={captions[index % captions.length]}
-              className="w-full"
-              imageUrl={`home/game_posters/slide_${index}.png`}></ImageCard>
-          </div>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-  </Carousel>
+  const items = [
+    { caption: "Rasayan: Compound Building Game", image: "home/game_posters/slide_0.png" },
+    { caption: "Ballebaaz: Trigonometry Puzzle", image: "home/game_posters/slide_1.png" },
+    { caption: "Aanganwadi: Virtual Village", image: "home/game_posters/slide_2.png" },
+    { 
+      caption: "Fun Lab", 
+      image: "home/game_posters/slide_3.png", 
+      link: "https://sandboxels.r74n.com/" 
+    },
+  ];
+
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {items.map((item, index) => (
+          <CarouselItem key={index}>
+            <div className="p-[10px] flex flex-col items-center w-full">
+              {item.link ? (
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="w-full"
+                >
+                  <ImageCard
+                    caption={item.caption}
+                    className="w-full"
+                    imageUrl={item.image}
+                  />
+                </a>
+              ) : (
+                <ImageCard
+                  caption={item.caption}
+                  className="w-full"
+                  imageUrl={item.image}
+                />
+              )}
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
 }
