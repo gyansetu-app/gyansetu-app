4cd0ac2f37427e923f9f300b3c5e492e544902b0
sortedcord <mail@adityagupta.dev>
2025-09-26 22:19:50 +0530

    feat: Added Login and onboarding

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import LeaderPage from "./pages/LeaderPage.tsx";
 import LearnPage from "./pages/LearnPage.tsx";
 import BottomBar from "./components/BottomBar.tsx";
 import QuizPage from "./pages/Quizpage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import OnboardingPage from "./pages/OnboardingPage.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
+function App() {
+  const location = useLocation();
+  const hideBottomBar =
+    location.pathname === "/login" || location.pathname === "/onboarding";
+
+  return (
+    <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/learn" element={<LearnPage />} />
         <Route path="/leaderboard" element={<LeaderPage />} />
         <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
-      <BottomBar />
+      {!hideBottomBar && <BottomBar />}
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
